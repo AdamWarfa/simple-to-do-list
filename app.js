732d741530160f3bdd8ebf219c9be6a8dcd9f510
AdamWarfa <Adamwarfadk@gmail.com>
2023-03-20 18:09:55 +0100

    dom manip

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 "use strict";
+window.addEventListener("load", initApp);
+const newToDo = document.querySelector("#input-new-to-do");
+const list = document.querySelector("#to-do-list");
+
+function initApp() {
+  document.querySelector("#btn-add-item").addEventListener("click", addToDo);
+}
+
+function addToDo() {
+  const listItem = document.createElement("li");
+  const listText = document.createElement("span");
+  const listBtn = document.createElement("button");
+
+  listText.textContent = newToDo.value;
+  listBtn.textContent = "DELETE";
+
+  listItem.appendChild(listText);
+  listItem.appendChild(listBtn);
+
+  list.appendChild(listItem);
+  newToDo.value = "";
+  newToDo.focus();
+
+  listBtn.addEventListener("click", removeToDo);
+}
+
+function removeToDo() {
+  this.parentNode.remove();
+}
